cda7be04fda556bd21580049f8c76a9e0ade6033
paleonia <ncdinosasd@gmail.com>
2025-09-01 11:31:28 +0000

    Deploying to gh-pages from @ paleonia/paleonia.github.io@da347ad08c1a36ebe65bb1fe71ffabfe808e4af2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,16 +419,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-co-authored-paper-a-carapace-of-pelodiscus-cf-p-gracilia-testudines-pan-trionychidae-from-the-middle-miocene-hagjeon-formation-of-pohang-si-gyeongsangbuk-do-south-korea-is-now-published",
           title: 'Co-authored paper “A carapace of Pelodiscus cf. P. gracilia (Testudines: Pan-Trionychidae) from the...',
+          description: "",
+          section: "News",},{id: "news-participating-in-the-6th-international-symposium-on-asian-dinosaurs-in-japan-isad-2025-as-a-poster-presentor",
+          title: 'Participating in The 6th International Symposium on Asian Dinosaurs in Japan(ISAD) 2025 as...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
