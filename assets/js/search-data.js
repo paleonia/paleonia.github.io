521b42b586c719e0c3bc31346b370c93e33c51c0
paleonia <ncdinosasd@gmail.com>
2025-10-09 07:10:25 +0000

    Deploying to gh-pages from @ paleonia/paleonia.github.io@4c88c0896e12b7958f08ef08c4a79432ddefdb5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,6 +407,17 @@ ninja.data = [{
             window.location.href = "/blog/2015/images/";
           
         },
+      },{id: "post-a-post-with-formatting-and-links",
+        
+          title: "a post with formatting and links",
+        
+        description: "march &amp; april, looking forward to summer",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/formatting-and-links/";
+          
+        },
       },{id: "fieldworks-national-dinosaur-fossil-excavation-project",
           title: 'National Dinosaur Fossil Excavation Project',
           description: "Paleontology Field Worker",
