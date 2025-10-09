d155d211685c9e3ae206f3ee408eec498d5ce966
paleonia <ncdinosasd@gmail.com>
2025-10-09 06:20:29 +0000

    Deploying to gh-pages from @ paleonia/paleonia.github.io@4b3276cfc349387ef3fc93902fde62017cfcc9a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,35 +472,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%65%61%67%6C%65%74%77@%79%6F%6E%73%65%69.%61%63.%6B%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://instagram.com/_twyoon", "_blank");
         },
       },{
       id: 'light-theme',
